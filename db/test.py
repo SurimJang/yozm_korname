f039e84589db0aa62cc0e048b23a7f734e338c81
SurimJang <think.percento@gmail.com>
2022-11-17 00:05:25 +0900

    Update

--- db/test.py
+++ db/test.py
@@ -1,11 +1,26 @@
+# Requirments
+#---------------------------
+
 from crawler import *
+import pickle
 import pandas as pd
 pd.set_option('display.max_columns', None)
 #import datetime as dt
 
 
-
-### Test
+# Load & SAVE
+#---------------------------
 data = collector(year='2022', gender='t', page='max')
-print(len(data))
-print(data[0].head())
+pickle.dump(data, open('name_all_2022.pkl', 'wb'))
+#data = pickle.load(open('name_all_2022.pkl', 'rb'))
+
+
+# Test
+#---------------------------
+
+cols = data[0].columns
+print(cols)
+
+for i in range(len(cols)):
+    print(type(data[0].iloc[0,i]))
+
