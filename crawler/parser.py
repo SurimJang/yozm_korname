4cde882c27983ff87b2be8ec27a131957e8621c3
SurimJang <think.percento@gmail.com>
2022-11-18 22:07:52 +0900

    Add namechart_parser as default

---
+++ crawler/parser.py
@@ -0,0 +1,31 @@
+def namechart_parser(response=None):
+    import re
+
+    data = []
+
+    table = driver.find_element(By.XPATH, '//*[@id="__next"]/div/div[2]/div[1]/div[3]/div[2]/table')
+    tbody = table.find_element(By.TAG_NAME, "tbody")
+    rows = tbody.find_elements(By.TAG_NAME, "tr")
+
+    for index, row in enumerate(rows):
+        row_name = row.find_elements(By.TAG_NAME, "td")[0]
+        row_rank = row.find_elements(By.TAG_NAME, "td")[1]
+        row_total = row.find_elements(By.TAG_NAME, "td")[2] 
+
+        name = row_name.text.strip()
+        ranks = row_rank.text.splitlines()
+        rank = re.sub(r'[^0-9]', '', ranks[0])
+        prevrank = ranks[1].replace('―', '0')
+        total = row_total.text.replace(',', '').replace('명', '').strip()
+    
+        result = {
+            "NO": index+1
+            , "NAME": name
+            , "RANK": int(rank)
+            , "PREVRANK": int(prevrank)
+            , "TOTAL": int(total)
+        }
+ 
+        data.append(result)
+ 
+    return data
